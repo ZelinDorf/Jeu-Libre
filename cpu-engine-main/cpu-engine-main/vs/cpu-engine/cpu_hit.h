d3e721741a5ff6595761836ed12baa845efd3675
ZelinDorf <iparisel@gaming.tech>
2026-03-16 15:56:45 +0100

    qzfjnkqs,vqf

--- cpu-engine-main/cpu-engine-main/vs/cpu-engine/cpu_hit.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct cpu_hit
-{
-public:
-	float dist;
-	XMFLOAT3 pt;
-};
--- cpu-engine-main/cpu-engine-main/vs/cpu-render/cpu_light.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-
-cpu_light::cpu_light()
-{
-	dir = { 0.5f, -1.0f, 0.5f };
-	XMStoreFloat3(&dir, XMVector3Normalize(XMLoadFloat3(&dir)));
-	ambient = 0.2f;
-}

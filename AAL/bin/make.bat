8102c727ca30daa7e2be04778918fc1062a4f7ee
ZelinDorf <iparisel@gaming.tech>
2026-03-17 16:33:33 +0100

    almost a legend

---
+++ AAL/bin/make.bat
@@ -0,0 +1,4 @@
+SolutionGenerator.exe -make ..\
+cd ../ide/vs
+start AAL.sln
+powershell.exe -command exit
---
+++ AAL/config/AAL.sol
@@ -0,0 +1,14 @@
+{
+    "format_version": "12.00",
+    "minimum_version": "10.0.40219.1",
+    "projects": [
+        "Core.prj",
+        "Engine.prj",
+        "Render.prj",
+        "Sandbox.prj",
+        "Gameplay.prj"
+    ],
+    "solution_name": "AAL",
+    "version": "16",
+    "version_full": "16.0.30319.14"
+}

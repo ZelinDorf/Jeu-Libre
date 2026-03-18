33d5813ba5ce0194a2285c8215c957df7e48a9f7
Lukas VUYLSTEKE <lvuylsteke@gaming.tech>
2026-03-18 09:36:31 +0100

    [ADD] enemy stats logic WIP

--- AAL/src/Gameplay/Enemy.cpp
+++ AAL/src/Gameplay/Enemy.cpp
@@ -1,12 +1,35 @@
 #include "pch.h"
 #include "Enemy.h"
 
-void Enemy::CreateEnemy(float hp, float dmg, float arm, float spd, float cost, int id)
+void Enemy::Create(float hp, float dmg, float arm, float spd, float cost, int id)
 {
-	m_health = hp;
-	m_damage = dmg;
-	m_armor = arm;
-	m_speed = spd;
-	m_cost = cost;
-	m_id = id;
+	// Determinate stats & cost
+	m_maxHealth = hp;	m_speed = spd;
+	m_id = id;		m_cost = cost;
+	m_armor = arm;	m_damage = dmg;
+	
+	// Determinate When can spawn
+
+	m_canAppear_v = { 0,1,3 }; // ex: map 0, 1 & 3
+
+	// 
+
 }
+
+void Enemy::ChangeStats(std::string stat, float value)
+{
+	if (stat == "maxHealth") {
+		m_maxHealth = value;
+	};
+	if (stat == "speed") {
+		m_speed = value;
+	};
+	if (stat == "armor") {
+		m_armor = value;
+	};
+	if (stat == "damage") {
+		m_damage = value;
+	};
+}
+
+
